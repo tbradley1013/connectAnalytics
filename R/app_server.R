b8a747739660143015b1320afa64ec90c3b7b6c5
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-24 14:13:16 -0500

    adding reactive values

--- R/app_server.R
+++ R/app_server.R
@@ -1,4 +1,10 @@
 #' @import shiny
 app_server <- function(input, output,session) {
+  session$onSessionEnded(stopApp)
+  
+  r <- shiny::reactiveValues(
+    connect_server = Sys.getenv("RSTUDIO_CONNECT_SERVER"),
+    api_key = Sys.getenv("RSTUDIO_CONNECT_API_KEY")
+  )
   # List the first level callModules here
 }
