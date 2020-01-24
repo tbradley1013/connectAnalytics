c96341d23c6dad8bc795d272020406608ef0c2a6
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-24 15:54:12 -0500

    js to enable enter to submit for the popups

---
+++ inst/app/www/button_click.js
@@ -0,0 +1,12 @@
+$(document).keyup(function(event) {
+  if ($("#01_server_connect_ui_1-api_key").is(":focus") && (event.key == "Enter")) {
+    $("#01_server_connect_ui_1-connect").click();
+  }
+});
+
+
+$(document).keyup(function(event) {
+  if ($("#02_username_ui_1-username").is(":focus") && (event.key == "Enter")) {
+    $("#02_username_ui_1-submit").click();
+  }
+});
