b4b9fe8ec45c47d2d9d77caad739ae51faf022f9
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-24 14:57:20 -0500

    namespace stuff

--- R/run_app.R
+++ R/run_app.R
@@ -4,8 +4,8 @@
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
 run_app <- function(...) {
-  with_golem_options(
-    app = shinyApp(ui = app_ui, server = app_server), 
+  golem::with_golem_options(
+    app = shiny::shinyApp(ui = app_ui, server = app_server), 
     golem_opts = list(...)
   )
 }
