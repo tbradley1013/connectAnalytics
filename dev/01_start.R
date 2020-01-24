7bc2f997372cdb2003a704009e1ef7d78b9b55bd
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-24 12:31:52 -0500

    chaning 01_start

--- dev/01_start.R
+++ dev/01_start.R
@@ -15,7 +15,7 @@ golem::fill_desc(
   author_first_name = "Tyler", # Your First Name
   author_last_name = "Bradley",  # Your Last Name
   author_email = "tyler.bradley@phila.gov",      # Your Email
-  repo_url = NULL # The (optional) URL of the GitHub Repo
+  repo_url = "https://github.com/tbradley1013/connectAnalytics" # The (optional) URL of the GitHub Repo
 )     
 
 ## Use this desc to set {golem} options
@@ -26,7 +26,7 @@ golem::set_golem_options()
 ## 
 ## If you want to use the MIT licence, README, code of conduct, lifecycle badge, and news
 
-usethis::use_mit_license( name = "Golem User" )  # You can set another licence here
+usethis::use_mit_license( name = "Tyler Bradley" )  # You can set another licence here
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
@@ -37,7 +37,7 @@ usethis::use_git()
 ## 1.3 - Add a data-raw folder
 ## 
 ## If you have data in your package
-usethis::use_data_raw( name = "my_dataset", open = FALSE ) # Change "my_dataset"
+# usethis::use_data_raw( name = "my_dataset", open = FALSE ) # Change "my_dataset"
 
 ## 1.4 - Init Tests
 ## 
