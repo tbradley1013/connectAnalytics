224b1a26781dcc9951155a4534af67fb2b5f4f99
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-24 15:41:11 -0500

    adding additional arguments to username module

--- R/app_server.R
+++ R/app_server.R
@@ -11,5 +11,5 @@ app_server <- function(input, output,session) {
   # List the first level callModules here
   callModule(mod_01_server_connect_server, "01_server_connect_ui_1", session = session, r = r)
   
-  callModule(mod_02_username_server, "02_username_ui_1")
+  callModule(mod_02_username_server, "02_username_ui_1", session = session, r = r)
 }
