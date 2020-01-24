67c2f624a69d7cfb44ceee2c08168fc6d59e7e2c
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-24 14:27:16 -0500

    working on login

--- R/mod_01_server_connect.R
+++ R/mod_01_server_connect.R
@@ -25,15 +25,40 @@ mod_01_server_connect_ui <- function(id){
 #' @rdname mod_01_server_connect
 #' @export
 #' @keywords internal
-    
 mod_01_server_connect_server <- function(input, output, session, r){
   ns <- session$ns
   
+  dialog <- modalDialog(
+    textInput(
+      inputId = ns("connect_server"),
+      label = "RStudio Connect Server",
+      value = (r$connect_server %||% "")
+    ),
+    passwordInput(
+      inputId = ns("api_key"),
+      label = "RStudio Connect API Key",
+      value = (r$connect_server %||% "")
+    ),
+    title = "Enter Connection Info",
+    footer = tagList(
+      modalButton("Cancel"),
+      actionButton(
+        ns("connect"),
+        "Connect!",
+        class = "btn-primary"
+      )
+    )
+  )
+  
   observe({
     if (is.null(r$connect_server) | is.null(r$api_key)){
-      
+      showModal(dialog)
+    } else {
+      r$client <- connectapi::connect(host = r$connect_server, api_key = r$api_key)
     }
   })
+  
+  observe({})
 }
     
 ## To be copied in the UI
