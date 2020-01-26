a40cb40d790505c71b5b97a2fc754d6355244871
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-25 22:04:39 -0500

    working on line graph

--- R/mod_05_usage.R
+++ R/mod_05_usage.R
@@ -26,6 +26,10 @@ mod_05_usage_ui <- function(id){
         ),
         style = "width = 300px;margin: 0 auto;"
       )
+    ),
+    fluidRow(
+      textOutput("temp_text"),
+      plotly::plotlyOutput(ns("usage_line_graph"))
     )
   )
 }
@@ -41,9 +45,55 @@ mod_05_usage_server <- function(input, output, session, r){
   
   observe({
     req(r$client, r$user_content)
+    # browser()
     
-    r$shiny_usage <- connectapi::get_usage_shiny(r$client, content_guid = )
+    r$shiny_usage <- connectapi::get_usage_shiny(
+      r$client, 
+      content_guid = r$user_content$guid,
+      from = input$content_dates[1], 
+      to = input$content_dates[2],
+      limit = Inf
+    )
+    
+    r$static_usage <- connectapi::get_usage_static(
+      r$client, 
+      content_guid = r$user_content$guid,
+      from = input$content_dates[1],
+      to = input$content_dates[2],
+      limit = Inf
+    )
   })
+  
+  output$temp_text <- renderText({
+    req(r$shiny_usage, r$static_content)
+    
+    return("Usage is loaded")
+  })
+  
+  output$usage_line_graph <- plotly::renderPlotly({
+    req(r$shiny_usage, r$static_usage)
+    
+    shiny_usage <- r$shiny_usage %>% 
+      dplyr::mutate(date = lubridate::date(started)) %>% 
+      dplyr::count(date, name = "n_shiny")
+    
+    static_usage <- r$static_usage %>% 
+      dplyr::mutate(date = lubridate::date(time)) %>% 
+      dplyr::count(date, name = "n_static")
+    
+    p <- shiny_usage %>% 
+      dplyr::left_join(
+        static_usage, 
+        by = "date"
+      ) %>% 
+      dplyr::mutate(total = sum(c(n_shiny, n_static), na.rm = TRUE)) %>% 
+      tidyr::pivot_longer(cols = n_shiny:total, values_drop_na = FALSE) %>% 
+      plotly::plot_ly(x = ~date, y = ~value, color = ~name) %>% 
+      plotly::add_lines()
+    
+    return(p)
+  })
+  
 }
     
 ## To be copied in the UI
