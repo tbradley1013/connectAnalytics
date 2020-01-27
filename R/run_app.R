2dc048484e60d0aebf928dc65fff98fcd5bc6a13
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-27 12:41:55 -0500

    switching host, api_key, and user info to the wrapper function as arguments

--- R/run_app.R
+++ R/run_app.R
@@ -1,11 +1,29 @@
 #' Run the Shiny Application
 #'
+#' @param host the host server for the RStudio Connect to be connected to. 
+#' Defaults to the environment variable "RSTUDIO_CONNECT_SERVER"
+#' @param api_key a valid RStudio Connect API key 
+#' @param user the desired user to log in as. If NULL (default) it will default
+#' to the session$user (recommended)
+#' @param switch_user logical; if TRUE a button appears in the navbar that will 
+#' allow users to switch which connect user analytics data they are looking at. 
+#' This could be useful if your company wants to allow users to see how others are
+#' performing. 
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
-connectAnalytics <- function(...) {
+connectAnalytics <- function(..., host = Sys.getenv("RSTUDIO_CONNECT_SERVER"), 
+                             api_key = Sys.getenv("RSTUDIO_CONNECT_API_KEY"),
+                             user = NULL, switch_user = TRUE) {
   golem::with_golem_options(
     app = shiny::shinyApp(ui = app_ui, server = app_server), 
-    golem_opts = list(...)
+    golem_opts = list(host = host, api_key = api_key, user = user, switch_user = switch_user, ...)
+  )
+}
+
+
+tmp_fn <- function(host = "test", ...){
+  return(
+    list(host = host, ...)
   )
 }
