794111b456389b471c8fe3641fd8c39940e570dc
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-27 12:43:44 -0500

    switching initial reactive values to golem options

--- R/app_server.R
+++ R/app_server.R
@@ -3,9 +3,9 @@ app_server <- function(input, output,session) {
   session$onSessionEnded(stopApp)
   
   r <- shiny::reactiveValues(
-    connect_server = Sys.getenv("RSTUDIO_CONNECT_SERVER"),
-    api_key = Sys.getenv("RSTUDIO_CONNECT_API_KEY"),
-    username = "tbradley"
+    connect_server = golem::get_golem_options("host"),
+    api_key = golem::get_golem_options("api_key"),
+    username = ifelse(is.null(golem::get_golem_options("user")), session$user, golem::get_golem_options("user"))
     # username = session$user
   )
   # List the first level callModules here
