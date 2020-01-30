791eabea2d960f6375e0deecff4e629046c1822d
Tyler Bradley <tyler.bradley@phila.gov>
2020-01-30 10:48:01 -0500

    added the admin page

--- R/mod_06_admin.R
+++ R/mod_06_admin.R
@@ -32,6 +32,32 @@ mod_06_admin_ui <- function(id){
         ),
         fluidRow(
           plotly::plotlyOutput(ns("admin_line_graph"))
+        ),
+        fluidRow(
+          shinydashboard::tabBox(
+            title = "Shiny Usage",
+            tabPanel(
+              title = "By Date",
+              plotly::plotlyOutput(ns("shiny_usage_by_date")) 
+            ),
+            tabPanel(
+              title = "By User",
+              plotly::plotlyOutput(ns("shiny_usage_by_user"))
+            )
+            
+          ),
+          shinydashboard::tabBox(
+            title = "Static Usage",
+            tabPanel(
+              title = "By Date",
+              plotly::plotlyOutput(ns("static_usage_by_date"))
+            ),
+            tabPanel(
+              title = "By User",
+              plotly::plotlyOutput(ns("static_usage_by_user"))
+            )
+          )
+          
         )
       )
     )
@@ -63,6 +89,44 @@ mod_06_admin_server <- function(input, output, session, r){
     req(r$shiny_usage_all, r$static_usage_all, r$admin)
     
     overall_usage_line(r$shiny_usage_all, r$static_usage_all, from = input$content_dates[1], to = input$content_dates[2], username = r$username, admin = TRUE)
+  })
+  
+  usage_shiny <- reactive({
+    req(r$shiny_usage_all, r$content, r$all_users)
+    
+    usage_info_join(r$shiny_usage_all, r$content, r$all_users)
+  })
+  
+  usage_static <- reactive({
+    req(r$static_usage_all, r$content, r$all_users)
+    
+    usage_info_join(r$static_usage_all, r$content, r$all_users)
+  })
+  
+  output$shiny_usage_by_date <- plotly::renderPlotly({
+    req(usage_shiny())
+    
+    usage_by_date(usage_shiny(), time_col = started, type = "Shiny App")
+    
+  })
+  
+  
+  output$shiny_usage_by_user <- plotly::renderPlotly({
+    req(usage_shiny())
+    
+    usage_by_user(usage_shiny(), type = "Shiny App")
+  })
+  
+  output$static_usage_by_date <- plotly::renderPlotly({
+    req(usage_static())
+    
+    usage_by_date(usage_static(), time_col = time, type = "Static Content")
+  })
+  
+  output$static_usage_by_user <- plotly::renderPlotly({
+    req(usage_static())
+    
+    usage_by_user(usage_static(), type = "Static Content")
   })
 }
     
