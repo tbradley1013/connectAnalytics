52bbe83d4bd0c6a4c732e090aec25bca02551e24
Tyler Bradley <tyler.bradley@phila.gov>
2020-02-03 16:08:49 -0500

    moving the inputs out of the dropdown

--- R/app_ui.R
+++ R/app_ui.R
@@ -12,31 +12,6 @@ ca_ui <- function() {
     shinyjs::hidden(
       div(
         id = "app-content",
-        # navbarPageWithInputs(
-        #   "connectAnalytics",
-        #   id = "navbar-tabs",
-        #   theme = shinythemes::shinytheme("flatly"),
-        #   tabPanel(
-        #     title = "Content",
-        #     icon = icon("user"),
-        #     mod_04_content_ui("04_content_ui_1"), # This module has no UI - it is simply loading the users content
-        #     mod_05_usage_ui("05_usage_ui_1", admin = FALSE)
-        #   ),
-        #   tabPanel(
-        #     title = "User Info",
-        #     icon = shiny::icon("user"),
-        #     mod_03_userinfo_ui("03_userinfo_ui_1")
-        #   ),
-        #   tabPanel(
-        #     title = "Admin",
-        #     icon = icon("toolbox"),
-        #     mod_05_usage_ui("05_usage_ui_2_admin", admin = TRUE)
-        #   ),
-        #   inputs = tagList(
-        #     mod_01_server_connect_ui("01_server_connect_ui_1"),
-        #     mod_02_username_ui("02_username_ui_1")
-        #   )
-        # )
         shinydashboard::dashboardPage(
           title = "connectAnalytics",
           # skin = "black",
@@ -60,20 +35,30 @@ ca_ui <- function() {
                 tabName = "admin",
                 icon = icon("users-cog")
               ),
-              shinydashboard::menuItem(
-                "Change Inputs",
-                tabName = "change_inputs",
-                dateRangeInput(
-                  inputId = "content_dates",
-                  label = "Select Date Range",
-                  start = (Sys.Date() - lubridate::days(7)),
-                  end = Sys.Date()
-                ),
-                # br(),
-                mod_02_username_ui("02_username_ui_1"),
-                # br(),
-                mod_01_server_connect_ui("01_server_connect_ui_1")
-              )
+              dateRangeInput(
+                inputId = "content_dates",
+                label = "Select Date Range",
+                start = (Sys.Date() - lubridate::days(7)),
+                end = Sys.Date()
+              ),
+              # br(),
+              mod_02_username_ui("02_username_ui_1"),
+              # br(),
+              mod_01_server_connect_ui("01_server_connect_ui_1")
+              # shinydashboard::menuItem(
+              #   "Change Inputs",
+              #   tabName = "change_inputs",
+              #   dateRangeInput(
+              #     inputId = "content_dates",
+              #     label = "Select Date Range",
+              #     start = (Sys.Date() - lubridate::days(7)),
+              #     end = Sys.Date()
+              #   ),
+              #   # br(),
+              #   mod_02_username_ui("02_username_ui_1"),
+              #   # br(),
+              #   mod_01_server_connect_ui("01_server_connect_ui_1")
+              # )
             )
           ),
           shinydashboard::dashboardBody(
